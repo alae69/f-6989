326e3ac47c1b7393cf696107698b32c54fb467ba
alae69 <alae1alaebilaizi@gmail.com>
2025-06-28 14:40:18 +0100

    Update drizzle.config.ts
    gg

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.psql 'postgresql://neondb_owner:npg_q1z9sMVxvKyd@ep-purple-bush-a8ywb6z1-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require',
   },
 });
